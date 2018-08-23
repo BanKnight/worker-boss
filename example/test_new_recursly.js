024302d5a82a86f9842d05f09e065ef21ae7191c
BanSky <tufeigd@163.com>
2018-08-23 12:09:51 +0800

    add new example

---
+++ example/test_new_recursly.js
@@ -0,0 +1,55 @@
+let boss = require("../lib")
+let argv = process.argv
+
+if (argv.length < 3)
+{
+    console.log("required more args")
+    return
+}
+
+let as_father = async function ()
+{
+    await boss.init(1)
+
+    let id = await boss.new_worker(__filename, "son")
+
+    boss.send_worker(id, "ping")
+}
+
+let as_son = async function ()
+{
+    let id = await boss.new_worker(__filename, "child")
+
+    boss.send_worker(id, "test")
+}
+
+let as_child = function ()
+{
+    setTimeout(() =>
+    {
+        boss.die()
+    }, 3000)
+}
+
+boss.on_dispatch(function (func_name, ...args)
+{
+    console.debug(func_name, ...args)
+})
+
+let start = async () =>
+{
+    switch (argv[2])
+    {
+        case "father":
+            as_father()
+            break
+        case "son":
+            as_son()
+            break
+        case "child":
+            as_child()
+            break
+    }
+}
+
+start()
