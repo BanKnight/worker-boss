40f27ad70cd3edd10b1a65dc9359ba5405745f9e
BanSky <tufeigd@163.com>
2018-08-23 11:46:42 +0800

    重命名测试文件

--- example/test_worker_dispatch.js
+++ example/test_worker_dispatch.js
@@ -5,13 +5,15 @@ let as_father = async function ()
 {
     await boss.init(1)
 
-    let id = await boss.new_worker("./example/test_worker_send", "child")
+    let id = await boss.new_worker(__filename, "child")
     for (let i = 0, len = 1000; i < len; ++i)
     {
         boss.send_worker(id, "ping", i)
     }
 
-    console.debug(`has already send msgs`)
+    let ret = await boss.call_worker(id, "test", 10)
+
+    console.debug(`has already send msgs:${ret}`)
 }
 
 let as_child = function ()
@@ -19,9 +21,10 @@ let as_child = function ()
     boss.on_dispatch(function (func, ...args)
     {
         console.log(func, ...args)
+
+        return "ceshi"
     })
 
-    boss.die()
 }
 
 let start = async () =>
