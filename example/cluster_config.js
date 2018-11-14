46f529bc876c59665a0822df27bc4b4384cb229c
BanSky <tufeigd@163.com>
2018-11-14 11:13:50 +0800

    update awesome-async

--- example/cluster_config.js
+++ example/cluster_config.js
@@ -1,5 +1,6 @@
 const config = module.exports = {}
 
+const mode = "vm"
 const clusters = {
     [1]: "127.0.0.1:8221",
     [2]: "127.0.0.1:8222",
@@ -8,9 +9,12 @@ const clusters = {
 config.server = {
     cluster: 1,
     clusters: clusters,
+    mode: mode,
 }
 
 config.client = {
     cluster: 2,
     clusters: clusters,
+    mode: mode,
 }
+
